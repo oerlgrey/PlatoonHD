f8caefd92cc27b864eacbd44a5bd896c10da46f5
örlgrey <ochzoetna@gmail.com>
2022-07-03 21:06:57 +0200

    Update to Version 2.0.7

--- usr/lib/enigma2/python/Plugins/Extensions/PlatoonHD/PlatoonHD.py
+++ usr/lib/enigma2/python/Plugins/Extensions/PlatoonHD/PlatoonHD.py
@@ -217,7 +217,7 @@ class PlatoonHD(ConfigListScreen, Screen):
 		self["helperimage"] = Pixmap()
 		self["Canvas"] = CanvasSource()
 		self["description"] = StaticText()
-		self["version"] = StaticText("Version 2.0.6")
+		self["version"] = StaticText("Version 2.0.7")
 
 		list = []
 		ConfigListScreen.__init__(self, list)
